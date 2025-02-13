bb7fb1217245e27df36456c57c056eaadf384550
Varad J <varad.gcs@gmail.com>
2025-02-13 11:47:16 +0530

    Bring back camera

--- src/pages/components/FileUploader.js
+++ src/pages/components/FileUploader.js
@@ -377,6 +377,7 @@ export default function FileUploader() {
           <input
             type="file"
             accept="image/*"
+            capture="environment"
             onChange={handleFileChange}
             className="block w-full text-sm cursor-pointer mb-4"
           />
